06cecc2f6736d628c0b853ab7b66cfb284bfd44d
Владимир Афанасьев <vladimirafanasev@MacBook-Air-Vladimir.local>
2026-04-30 19:23:43 +0300

    chore: remove dist/.astro

--- dist/server/entry.mjs
+++
@@ -1,7 +0,0 @@
-export { h as handler, o as options, a as startServer } from './chunks/server_CG42zlcv.mjs';
-import './chunks/sequence_C7YAHkIp.mjs';
-import '@astrojs/internal-helpers/path';
-import '@astrojs/internal-helpers/remote';
-import 'piccolore';
-import 'es-module-lexer';
-import 'clsx';
